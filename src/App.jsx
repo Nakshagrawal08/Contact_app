41d1112a1126ae0258a3f694f113c9a0d8b58036
nakshagrawal08 <kanhaagrawal080604@gmail.com>
2024-06-02 03:25:21 +0530

    other changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,56 +5,67 @@ import List from './list'
 
 function App() {
   const [input , Setinput] = useState()
-  const [input2 , Setinput2] = useState()
+  const [contact1 , Setcontact1] = useState()
+  const [contact2 , Setcontact2] = useState()
+  const [email , Setemail] = useState()
+  const [address , Setaddress] = useState()
   const [result , Setresult] = useState([])
   
  
   function addTask(){
    result.push({
      Name : input,
-     Contact : input2 ,
+     Contact1 : contact1 ,
+     Contact2 : contact2 ,
+     Mail : email ,
+     Address : address 
    })
  Setresult([...result])
  Setinput("")
- Setinput2("")
+ Setcontact1("")
+ Setcontact2("")
+ Setemail("")
+ Setaddress("")
   }
   return (
-    <><div className='flex m-4 p-2 '>
-    <div className='grid grid-cols-1 gap-3 h-fit w-1/4 bg-gray-500 rounded-md'>
-      <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Search Contact</button>
-      <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Show Contact</button>
-      <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Show Duplicates</button>
-      <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Search Starred Contact</button>
-      <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Search Contact</button>
+    <>
+  <div className='flex m-4 p-2 '>
+        {/* CODE FOR BOTTONS */}
+      < div className='grid grid-cols-1 gap-3 h-fit w-1/4 shadow-slate-800 bg-gray-500 rounded-md'>
+        <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Search Contact</button>
+        <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Show Contact</button>
+        <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Show Duplicates</button>
+        <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Search Starred Contact</button>
+        <button className='p-2 m-2 text-white text-2xl bg-slate-800 rounded-md text-left'>Show Blocklist</button>
      </div>
-    <div className='w-3/4 shadow-xl ml-4 shadow-slate-900  bg-gray-500 rounded-md'>
+         {/* CODE FOR FORM*/}
+    <div className='w-3/4 shadow-xl ml-4 shadow-slate-800  bg-gray-500 rounded-md'>
       <div className=' p-2 m-8 '>
           <div className='my-3' >
-           <input type="text" name="Name" id="name" placeholder='Enter Name'value={input} onChange={(e) => Setinput(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-black w-full ' />
+           <input type="text" name="Name" id="name" placeholder='Enter Name'value={input} onChange={(e) => Setinput(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-white w-full ' />
           </div>
           <div className='my-3' >
-            <input type="text" name="Number1" id="number1" placeholder='Enter 1st Number' value={input2} onChange={(e) => Setinput2(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-black w-full ' />
+            <input type="text" name="Number1" id="number1" placeholder='Enter 1st Number' value={contact1} onChange={(e) => Setcontact1(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-white w-full ' />
           </div>
           <div className='my-3' >
-            <input type="text" name="Number2" id="number2" placeholder='Enter 2nd Number' value={input2} onChange={(e) => Setinput2(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-black w-full ' />
+            <input type="text" name="Number2" id="number2" placeholder='Enter 2nd Number' value={contact2} onChange={(e) => Setcontact2(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-white w-full ' />
           </div>
           <div className='my-3' >
-            <input type="text" name="Email" id="email" placeholder='Enter Email'value={input} onChange={(e) => Setinput(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-black w-full ' />
+            <input type="email" name="Email" id="email" placeholder='Enter Email'value={email} onChange={(e) => Setemail(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-white w-full ' />
           </div>
           <div className='my-3' >
-           <input type="text" name="Address" id="address" placeholder='Enter Address'value={input} onChange={(e) => Setinput(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-black w-full ' />
-          </div>
-          
+           <input type="text" name="Address" id="address" placeholder='Enter Address'value={address} onChange={(e) => Setaddress(e.target.value)} className='p-2 text-2xl bg-slate-800 rounded-md text-white w-full ' />
+          </div>         
           <div className=' content-center '>
-            <button onClick={addTask}className=' bg-red-400 py-2 px-12 rounded-md'>Add</button>
+            <button onClick={addTask}className=' bg-red-400 text-white text-lg py-2 px-12 rounded-md'>Add</button>
           </div>
-        </div>
-        <div>
-          <List data={result} />
         </div>
       </div>
     </div>
-   
+    {/* CODE FOR LIST */}
+      <div className='grid grid-cols-4 gap-3 mx-8'>
+          <List data={result} />
+        </div>
     </>
   )
 }
--- src/list.jsx
+++ src/list.jsx
@@ -4,8 +4,12 @@ export default function ({data}) {
   return (<>
   {
         data.map((e)=>
-            <div className='bg-blue-300 p-2'>
-                {e.Name} - {e.Contact}
+            <div className='bg-gray-500 rounded-md my-3  px-4 py-2'>
+              <h1 className=' font-bold text-lg text-white'>{e.Name}</h1> 
+              <h2 className=' tet-md text-white'>{e.Contact1}</h2>
+              <h2 className=' tet-md text-white'>{e.Contact2}</h2>
+              <h2 className=' tet-md text-white'>{e.Mail}</h2>
+              <h2 className=' tet-md text-white'>{e.Address}</h2>
             </div>
         )
     }
