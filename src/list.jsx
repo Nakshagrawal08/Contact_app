11582285da6e5806d512cbfd6c9b249db36fcaac
nakshagrawal08 <kanhaagrawal080604@gmail.com>
2024-06-02 21:24:10 +0530

    ch

--- src/list.jsx
+++ src/list.jsx
@@ -4,7 +4,7 @@ export default function ({data}) {
   return (<>
   {
           data.map((e)=>
-            <div className='bg-gray-500 rounded-md my-3  px-4 py-2'>
+            <div className='bg-gray-700 rounded-md m-3 px-4 py-2'>
               <h1 className=' font-bold text-lg text-white'>{e.Name}</h1> 
               <h2 className=' tet-md text-white'>{e.Contact1}</h2>
               <h2 className=' tet-md text-white'>{e.Contact2}</h2>
